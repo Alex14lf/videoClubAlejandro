cea7d8908de82d3a814e468217e102d2ecc6a898
alex1 <alex14lf@gmail.com>
2024-02-10 13:30:04 +0100

    Creation of the function to connect to the database and the error404 page, along with their styles.

--- lib/functions/functions.php
+++ lib/functions/functions.php
@@ -1,7 +1,17 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+function ConectarBd() {
+    $cadena_conexion = 'mysql:dbname=clubdeportivo;host=127.0.0.1';
+    $usuario = 'root';
+    $clave = '';
+    try {
+        //Se crea la conexión con la base de datos
+        $bd = new PDO($cadena_conexion, $usuario, $clave);
+        return $bd;
+    } catch (Exception $e) {
+        header('Location: ../pages/error404.php');
+        exit();
+    }
+}
 
+?>
---
+++ pages/error404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Error 404 - Página no encontrada</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/error404.css">
+    </head>
+    <body>
+        <div class="container error-container">
+            <div class="row">
+                <div class="col text-center">
+                    <div class="error-heading">404</div>
+                    <div class="error-message">¡Oops! La página que buscas no pudo ser encontrada.</div>
+                    <a href="../index.php" class="error-link">Volver a la página principal</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
